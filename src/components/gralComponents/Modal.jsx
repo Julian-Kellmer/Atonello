a82f82bb61308bb7efc2467d96700533d34fe4ff
Julian-Kellmer <kellmer002@gmail.com>
2024-09-13 11:10:50 -0300

    resposinve modal

--- src/components/gralComponents/Modal.jsx
+++ src/components/gralComponents/Modal.jsx
@@ -37,9 +37,9 @@ const Modal = ({ isOpen, closeModal, slides }) => {
     <>
       {isOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-300">
-          <div className="bg-[ #FBF8F4] p-8 rounded-lg shadow-lg relative md:max-w-7xl mx-auto">
+          <div className="bg-[#FBF8F4] p-6 md:rounded-lg md:shadow-lg relative  md:max-w-7xl max-w-full mx-auto">
             <button
-              className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+              className="z-20 text-4xl absolute top-4 right-4 text-gray-500 hover:text-gray-700"
               onClick={closeModal}
             >
               X
@@ -56,11 +56,11 @@ const Modal = ({ isOpen, closeModal, slides }) => {
               {slides.map((slide, index) => (
                 <SwiperSlide
                   key={index}
-                  className="p-4 border rounded-lg bg-[#FBF8F4] shadow-md h-96 w-full"
+                  className="p-4 md:border rounded-lg bg-[#FBF8F4] md:shadow-md h-full w-full "
                 >
-                  <div className="grid grid-cols-1 md:grid-cols-10 gap-4">
-                    {/* aca comiensa las fotitos del costado */}
-                    <div className="col-span-1 md:col-span-1 bg-[ #FBF8F4] p-4">
+                  <div className="grid grid-cols-1 md:grid-cols-10 gap-4 ">
+                    {/* Left: Sidebar images */}
+                    <div className="col-span-1 md:col-span-1 bg-[#FBF8F4] p-4 hidden md:block">
                       {slide.colors.map((color, idx) => (
                         <img
                           key={idx}
@@ -71,8 +71,11 @@ const Modal = ({ isOpen, closeModal, slides }) => {
                         />
                       ))}
                     </div>
-                    {/* aca comienza las fotos del traje que hacen slide */}
-                    <div className={`${fadeState} col-span-1 md:col-span-4 bg-[#FBF8F4] p-4`}>
+
+                    {/* Center: Main Swiper */}
+                    <div
+                      className={`${fadeState}  col-span-1 md:col-span-4 bg-[#BF8F4] p-4`}
+                    >
                       <Swiper
                         spaceBetween={30}
                         centeredSlides={true}
@@ -91,29 +94,36 @@ const Modal = ({ isOpen, closeModal, slides }) => {
                         </SwiperSlide>
                       </Swiper>
                     </div>
-                    {/* contenido a modificar */}
-                    <div className={`${fadeState} col-span-1 md:col-span-5 bg-[#FBF8F4] h-96 p-4 `}>
-                      <h2 className="text-5xl font-serif pb-4">
+
+                    {/* Right: Details */}
+                    <div
+                      className={`${fadeState} col-span-1 md:col-span-5 bg-[#FBF8F4] h-3/4 p-4 `}
+                    >
+                      <h2 className="text-3xl md:text-5xl font-serif pb-4">
                         {slide.modalTitle}
                       </h2>
-                      <div className="w-full h-0.5 bg-[#bcad7r] "></div>
-                      <p>{slide.modalDescription}</p>
-                      <div className="border w-3/6 flex flex-row">
-                        <div className="h-24 w-24 border"> image</div>
-                        <div className="bg-white flex-1">
+                      <div className="w-full h-1 bg-[#BCAD7E] mb-4"></div>
+                      <p className="text-sm md:text-base">{slide.modalDescription}</p>
+                      <div className="border w-full md:w-3/6 flex flex-col md:flex-row mt-6">
+                        <div className="h-24 w-full border">Image</div>
+                        <div className="bg-white flex-1 p-4">
                           <ul className="list-disc pl-5">
-                            <p className="text-[#BCAD7E] font-medium text-lg font-serif ">{"Color"}</p>
+                            <p className="text-[#BCAD7E] font-medium text-lg font-serif ">
+                              {"Color"}
+                            </p>
                             <li className="text-base list-none text-sm font-semibold">
                               {slide.colors[selectedIndex]}
                             </li>
                           </ul>
-                          <ul className="list-disc pl-5">
-                            <p className="text-[#BCAD7E] font-medium font-serif">{"Material"}</p>
+                          <ul className="list-disc pl-5 mt-2">
+                            <p className="text-[#BCAD7E] font-medium font-serif">
+                              {"Material"}
+                            </p>
                             <li className="text-base list-none text-sm font-semibold">
                               {slide.materials[selectedIndex]}
                             </li>
                           </ul>
-                          <div className="w-full h-[5px] bg-[#BCAD7E]"></div>
+                          <div className="w-full h-[5px] bg-[#BCAD7E] mt-4"></div>
                         </div>
                       </div>
                     </div>
